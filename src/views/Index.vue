7e48848277510c9cf4ec9efb0c063448865167a2
Shimada666 <649940882@qq.com>
2021-09-16 00:28:41 +0800

    feat: 主页底下的具体内容

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -19,14 +19,43 @@ import Logo from '@/components/Logo.vue'
           <div class="intro-text">这是第三行</div>
         </div>
         <div class="link">
-          博客&nbsp;&nbsp;>&nbsp;&nbsp;二狗&nbsp;&nbsp;>&nbsp;&nbsp;了解我们
+          博客&nbsp;c>&nbsp;&nbsp;二狗&nbsp;&nbsp;>&nbsp;&nbsp;了解我们
         </div>
       </div>
     </div>
   </div>
-  <!--<div class="main">-->
-  <!--  -->
-  <!--</div>-->
+  <div class="main">
+    <div class="row">
+      <div class="left">
+        <div class="left-summary">最近工作</div>
+      </div>
+      <div class="right">
+        <div class="article">
+          <div class="article-title">第一篇文章: 小标题小标题</div>
+          <div class="article-content">简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介</div>
+          <div class="article-footer">
+            <span class="article-date">2021-9-11</span>
+            <span class="article-author">Lune</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="left">
+        <div class="left-summary">捍卫者</div>
+      </div>
+      <div class="right">
+        <div class="article">
+          <div class="article-title">第一篇文章: 小标题小标题</div>
+          <div class="article-content">简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介</div>
+          <div class="article-footer">
+            <span class="article-date">2021-9-11</span>
+            <span class="article-author">Lune</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -59,14 +88,14 @@ import Logo from '@/components/Logo.vue'
     }
 
     .title {
-      margin-top: 0;
+      margin-top: 8px;
       font-size: 64px;
       font-family: DaxlinePro-Medium, sans-serif;
       letter-spacing: 2px;
     }
 
     .intro {
-      margin-top: 24px;
+      margin-top: 16px;
 
       .intro-text {
         font-size: 18px;
@@ -77,6 +106,50 @@ import Logo from '@/components/Logo.vue'
     .link {
       margin-top: 24px;
       font-size: 18px;
+    }
+  }
+}
+
+.row {
+  margin-top: 64px;
+  display: flex;
+
+  .left {
+    display: block;
+    flex: 0 0 40%;
+    max-width: 40%;
+
+    .left-summary {
+      font-size: 36px;
+      line-height: 1.2;
+    }
+  }
+
+  .right {
+    display: block;
+    flex: 0 0 60%;
+    max-width: 60%;
+
+    .article {
+      .article-title {
+        font-size: 20px;
+        font-weight: bold;
+      }
+
+      .article-content {
+        margin-left: 20px;
+        margin-top: 10px;
+      }
+
+      .article-footer {
+        margin-left: 20px;
+        margin-top: 10px;
+        font-size: 14px;
+
+        .article-author {
+          margin-left: 16px;
+        }
+      }
     }
   }
 }
